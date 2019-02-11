2af5cc3bf3418d7c226042b1d2c561571e14c9dc
André Wagner <andre.nho@gmail.com>
2019-02-11 13:14:50 -0200

    .

---
+++ terraform/scripts.tf
@@ -0,0 +1,53 @@
+resource "null_resource" "scripts" {
+    triggers = {
+    }
+
+    depends_on = ["aws_instance.jenkins", "aws_eip.jenkins_eip"]
+
+    connection {
+        type    	= "ssh"
+        host        = "${aws_eip.jenkins_eip.public_ip}"
+        user    	= "ec2-user"
+        timeout 	= "30s"
+        private_key = "${file("jenkins_aws.pem")}"
+    }
+
+    provisioner "file" {
+        source      = "files/setup_jenkins_aws.sh"
+        destination = "/tmp/setup_jenkins_aws.sh"
+    }
+
+    provisioner "file" {
+        source      = "files/job.xml"
+        destination = "/tmp/job.xml"
+    }
+
+    provisioner "file" {
+        source      = "files/config.xml"
+        destination = "/tmp/config.xml"
+    }
+
+    provisioner "file" {
+        source      = "files/awscred.xml"
+        destination = "/tmp/awscred.xml"
+    }
+
+    provisioner "remote-exec" {
+        inline 		= [
+            "sudo chmod +x /tmp/setup_jenkins_aws.sh",
+            "sudo cp /tmp/job.xml /tmp/frontend.xml",
+            "sudo cp /tmp/job.xml /tmp/backend.xml",
+            "sudo sed -i 's,GIT_REPO,${var.git_repo_frontend},g' /tmp/frontend.xml",
+            "sudo sed -i 's,GIT_REPO,${var.git_repo_backend},g' /tmp/backend.xml",
+            "sudo sed -i 's,JENKINS_PASSWORD,${var.jenkins_password},g' /tmp/setup_jenkins_aws.sh",
+            "sudo sed -i 's,MY_APP_NAME,${var.basename},g' /tmp/setup_jenkins_aws.sh",
+            "sudo sed -i 's,MY_APP_NAME,${var.basename},g' /tmp/job.xml",
+            "sudo sed -i 's,MY_AWS_DOMAIN,${var.domain},g' /tmp/setup_jenkins_aws.sh",
+            "sudo sed -i 's,MY_KEY_ID,${var.access_key},g' /tmp/awscred.xml",
+            "sudo sed -i 's,MY_SECRET_KEY,${var.secret_key},g' /tmp/awscred.xml",
+            "sudo /tmp/setup_jenkins_aws.sh",
+        ]
+    }
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
