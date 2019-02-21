64f102c679124c876424167960867c77656ba7aa
Andre Wagner <andre.nho@gmail.com>
2019-02-21 17:00:45 -0300

    .

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,13 +1,8 @@
-data "aws_vpc" "default" {
-    default = true
-}
-
+# TODO - remove this
 resource "aws_security_group" "allow_ssh" {
     name            = "allow_ssh"
     description     = "Allow SSH inbound"
     
-    vpc_id          = "${aws_vpc.default.id}"
-
     ingress {
         from_port   = 22
         to_port     = 22
@@ -26,8 +21,6 @@ resource "aws_security_group" "allow_443" {
     name            = "allow_443"
     description     = "Allow 443 inbound"
 
-    vpc_id          = "${aws_vpc.default.id}"
-    
     ingress {
         from_port   = 443
         to_port     = 443
@@ -46,8 +39,6 @@ resource "aws_security_group" "allow_8080" {
     name            = "allow_8080"
     description     = "Allow 8080 inbound"
 
-    vpc_id          = "${aws_vpc.default.id}"
-    
     ingress {
         from_port   = 8080
         to_port     = 8080
@@ -66,8 +57,6 @@ resource "aws_security_group" "allow_outbound" {
     name            = "allow_all_outbound"
     description     = "Allow all traffic outbound"
 
-    vpc_id          = "${aws_vpc.default.id}"
-    
     egress {
         from_port   = 0
         to_port     = 0
